0d63a3f2d92701bdf2f746be3bce2f8235b2887b
Andre Moreno Goveia <90646603+AndreMorenoGoveia@users.noreply.github.com>
2023-06-23 06:18:00 -0300

    Erros corrigidos

--- src/Auxiliares/Icone.tsx
+++ src/Auxiliares/Icone.tsx
@@ -6,7 +6,8 @@ import * as ReactIconsFi from 'react-icons/fi';
 import * as ReactIconsDi from 'react-icons/di';
 import * as ReactIconsAi from "react-icons/ai";
 import * as ReactIconsBs from "react-icons/bs";
-import * as ReactIconsBi from "react-icons/bi"
+import * as ReactIconsBi from "react-icons/bi";
+import * as ReactIconsFc from "react-icons/fc";
 import './Icone.scss'
 
 interface IconeProps {
@@ -15,36 +16,97 @@ interface IconeProps {
         key: number
      }
 
+let key = 0
+
 export function Icone({iconName, animationDelay, key}: IconeProps) {
     const iconFamily = iconName.slice(0, 2);
     let Icon: IconType;
+    key++;
     switch (iconFamily) {
         case 'Fa':
-            Icon = ReactIconsFa[iconName as keyof typeof ReactIconsFa]
-            return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            try{
+                Icon = ReactIconsFa[iconName as keyof typeof ReactIconsFa]
+                return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            }
+            catch(e){
+                Icon = ReactIconsFa['FaCloud']
+                return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            }
         case 'Gi':
-            Icon = ReactIconsGi[iconName as keyof typeof ReactIconsGi]
-            return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            try{
+                Icon = ReactIconsGi[iconName as keyof typeof ReactIconsGi]
+                return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            }
+            catch(e){
+                Icon = ReactIconsFa['FaCloud']
+                return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            }
         case 'Io':
-            Icon = ReactIconsIo[iconName as keyof typeof ReactIconsIo]
-            return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            try{
+                Icon = ReactIconsIo[iconName as keyof typeof ReactIconsIo]
+                return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            }
+            catch(e){
+                Icon = ReactIconsFa['FaCloud']
+                return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            }
         case 'Fi':
-            Icon = ReactIconsFi[iconName as keyof typeof ReactIconsFi]
-            return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            try{
+                Icon = ReactIconsFi[iconName as keyof typeof ReactIconsFi]
+                return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            }
+            catch(e){
+                Icon = ReactIconsFa['FaCloud']
+                return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            }
         case 'Di':
-            Icon = ReactIconsDi[iconName as keyof typeof ReactIconsDi]
-            return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            try{
+                Icon = ReactIconsDi[iconName as keyof typeof ReactIconsDi]
+                return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            }
+            catch(e){
+                Icon = ReactIconsFa['FaCloud']
+                return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            }
         case 'Ai':
-            Icon = ReactIconsAi[iconName as keyof typeof ReactIconsAi]
-            return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            try{
+                Icon = ReactIconsAi[iconName as keyof typeof ReactIconsAi]
+                return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            }
+            catch(e){
+                Icon = ReactIconsFa['FaCloud']
+                return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            }
         case 'Bs':
-            Icon = ReactIconsBs[iconName as keyof typeof ReactIconsBs]
-            return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            try{
+                Icon = ReactIconsBs[iconName as keyof typeof ReactIconsBs]
+                return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            }
+            catch(e){
+                Icon = ReactIconsFa['FaCloud']
+                return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            }
         case 'Bi':
-            Icon = ReactIconsBi[iconName as keyof typeof ReactIconsBi]
-            return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            try{
+                Icon = ReactIconsBi[iconName as keyof typeof ReactIconsBi]
+                return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            }
+            catch(e){
+                Icon = ReactIconsFa['FaCloud']
+                return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            }
+        case 'Fc':
+            try{
+                Icon = ReactIconsFc[iconName as keyof typeof ReactIconsFc]
+                return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            }
+            catch(e){
+                Icon = ReactIconsFa['FaCloud']
+                return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
+            }
         default:
             Icon = ReactIconsFa['FaCloud']
             return <Icon key={key} size={40} className='Icone' style={{animationDelay: `${animationDelay}s`}}/>
     }
+    
 }
